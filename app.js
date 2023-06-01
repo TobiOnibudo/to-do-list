89b52b9f97fe657c16df013cf866322729c7e3d4
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-06-01 18:21:41 -0300

    Added functionality and styling

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-//jshint jsesversion6
+//jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.listen(3000, 
     function(){
     console.log("Server started on port 3000"); }
 );
 
+let items = ["Buy Food","Cook Food","Eat Food"];
+
 app.get("/", function(req,res)
 {
-    res.send("Welcome to my website");
+    let today = new Date();
+   
+    let options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day,newListItems: items });
+   
+});
+
+app.post("/", function(req,res)
+{
+    let item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
 });
