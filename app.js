305dc34c20628543dc1fbd63999f53012f94c2f9
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-06-01 21:27:31 -0300

    created date module

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname+ "/date.js");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -18,15 +18,8 @@ let items = ["Buy Food","Cook Food","Eat Food"];
 let workItems = [];
 app.get("/", function(req,res)
 {
-    let today = new Date();
    
-    let options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
     res.render("list", {listTitle: day,newListItems: items });
    
 });
